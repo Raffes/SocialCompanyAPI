570405c4688909ea1bc9b869f1cf08de5e10e9b7
Rafaela <rafaelaperes215@gmail.com>
2025-03-27 10:42:43 -0300

    feat: Adicionado requisição para atualizar acessos do post

---
+++ endpoints/accesses_patch.js
@@ -0,0 +1,36 @@
+const { admin } = require("../auth/auth-admin.js");
+
+const db = admin.firestore();
+
+const updateAccessesPost = async (req, res) => {
+  try {
+    const { user, postId } = req.query;
+    
+    const postRef = db.collection("posts").doc(postId);
+    const postDoc = await postRef.get();
+
+    if(!postDoc.exists) {
+      throw new Error("Post não encontrado!")
+    }
+
+    if(postDoc.data().userId !== user) {
+      throw new Error("Sem permissão para fazer essa ação")
+    }
+
+    await postRef.update({
+      acessos: postDoc.data().acessos + 1,
+      updatedAt: admin.firestore.FieldValue.serverTimestamp()
+    })
+
+    const postUpdated = await postRef.get();
+
+    res.status(200).json({
+      posts: postUpdated.data()
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = updateAccessesPost;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const signOut = require("./endpoints/user_signOut.js");
 const getUser = require("./endpoints/user_get.js");
 const createPost = require("./endpoints/photo_post.js");
 const listPostsById = require("./endpoints/photo_get.js");
+const updateAccessesPost = require("./endpoints/accesses_patch.js");
 
 const expressApp = express();
 expressApp.use(cors());
@@ -35,6 +36,9 @@ expressApp.post("/api/createPost", upload.single("imageFile"), createPost);
 
 // Endpoint de pegar post do usuário
 expressApp.get("/api/listPostsById", listPostsById);
+
+// Endpoint de atualizar acessos do post
+expressApp.patch("/api/updateAccessesPost", updateAccessesPost);
 
 expressApp.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
